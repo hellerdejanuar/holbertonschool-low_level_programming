1bfe75850885dc28a189799683ed700c8b54ff49
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-03 17:58:33 -0800

    6 updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,19 +7,19 @@
  * Return: 0 success
  */
 
-char charSize;
-int intSize;
-long int longSize;
-long long int longlongSize;
-float floatSize;
-
 int main(void)
 {
+	char charSize;
+	int intSize;
+	long int longSize;
+	long long int longlongSize;
+	float floatSize;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(charSize));
 	printf("Size of an int: %lu byte(s)\n", sizeof(intSize));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(longSize));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongSize));
 	printf("Size of a float: %lu byte(s)\n", sizeof(floatSize));
-	
+
 	return (0);
 }
