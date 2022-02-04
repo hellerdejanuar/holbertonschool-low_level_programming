d76815e22c9e54bf2612d3a5c8e43cd06ffbd2ef
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-03 17:50:12 -0800

    6 done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints Variable Type sizes
+ *
+ * Return messages
+ * Return: 0 success
+ */
+
+char charSize;
+int intSize;
+long int longSize;
+long long int longlongSize;
+float floatSize;
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(charSize));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intSize));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longSize));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongSize));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floatSize));
+	
+	return (0);
+}
