6d82d44972bd759c4c5dc5ab7c55ee6513e3d407
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-04 11:40:30 -0800

    6 completed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - Print numbers
+ *   *
+ *    * Return: (0) success
+ *     */
+int main(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%i", n);
+	}
+		putchar('\n');
+
+	return (0);
+}
