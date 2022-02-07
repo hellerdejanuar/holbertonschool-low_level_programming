d08b4ad1c26b15113db907a51b401d76f45e5517
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-07 10:24:02 -0800

    2 created

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the entire alphabet
+ */
+
+void print_alphabet(void)
+	{
+		char alpha;
+		int x;
+
+		for (x = 0; x < 10; x++)
+			for (alpha = 'a'; alpha <= 'z'; alpha++)
+			{
+				_putchar(alpha);
+			}
+		_putchar('\n');
+	}
