ebb893d1fbc0d7fc1bb5d273cd71cf06fb7c4cfa
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-07 11:30:31 -0800

    5 done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of int
+ * @n: character (in int) input to be checked
+ * Return: 1 positive. 0 zero. -1 negative
+ */
+
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (c == 0)
+			return (0);
+		else
+			return (-1);
+	}
+
+	_putchar('\n');
+}
