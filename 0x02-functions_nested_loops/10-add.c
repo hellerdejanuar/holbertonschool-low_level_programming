bfc4303860007d613f69335646d20167b7d28be5
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-08 12:54:41 -0800

    10 updated

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,13 +4,13 @@
  * add - adds a to b
  * @a: number 1
  * @b: number 2
- * return: numbers 1 and 2 added together
+ * return - (n) numbers 1 and 2 added together
  */
 
 int add(int a, int b)
 {
 	int n;
-	
+
 	n = a + b;
 	return (n);
 }
