7e2b97d4bd9319f399d8510236aea75d1a1eaa20
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-08 12:59:04 -0800

    10 style corrected

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,9 @@
  * add - adds a to b
  * @a: number 1
  * @b: number 2
- * return: (n) numbers 1 and 2 added together
+ *
+ * Return Messages
+ * Return: n numbers a and b added together
  */
 
 int add(int a, int b)
