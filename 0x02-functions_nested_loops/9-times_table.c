60b1a67f0f281cb44d30ae479a47222d4a8ddfe4
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-07 18:57:17 -0800

    9 created

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - prints all numbers times all others from 0 to 10
+ */
+
+void times_table(void)
+{
+	int col;
+	int row;
+	int firstD;
+	int secD;
+	int x;
+		
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			x = col * row;
+			if (x > 9)
+			{
+				secD = x % 10;
+				firstD = (x - secD) / 10;
+				_putchar(firstD + '0');
+				_putchar(secD + '0');
+			}
+			else 
+				_putchar(x + '0');
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('$');
+				_putchar('\n');
+			}
+		}
+	}
+}
+
