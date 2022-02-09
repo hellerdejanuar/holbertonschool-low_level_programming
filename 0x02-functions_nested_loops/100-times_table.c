3912d88daa490b7974f6be415a89197c2674491b
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-08 17:47:52 -0800

    11 test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,48 @@
+#include "main.h"
 #include <math.h>
-#include <stdio.h>
 
-int main(void)
+/**
+ * times_table - prints all numbers times all others from 0 to 10
+ */
+
+void print_times_table(void)
 {
-	int n;
+	int col;
+	int row;
+	int firstD;
+	int secD;
+	int x;
+	int intlen;
 
-	n = log(100);
-return (n);
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			x = col * row;
+			if (x > 9)
+			{
+				secD = x % 10;
+				firstD = (x - secD) / 10;
+				_putchar(firstD + '0');
+				_putchar(secD + '0');
+			}
+			else
+			{
+				if (col != 0)
+					_putchar(' ');
+
+				_putchar(x + '0');
+			}
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+				intlen = log(10);
+			}
+		}
+	}
 }
