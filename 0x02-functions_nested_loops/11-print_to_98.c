ee63d77bdbd56db45ea27ba9cfc65f315e6e1791
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-08 17:04:43 -0800

    11 created

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * add - adds a to b
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return Messages
+ * Return: n numbers a and b added together
+ */
+
+void print_to_98(int n)
+{
+		int cn;
+		int targ = 98;
+
+		if (n < targ)
+		{
+			for (cn = n; cn < targ; cn++)
+				{
+					printf("%i, ", cn);
+				}
+		}
+
+		else if (n > targ)
+		{
+			for (cn = n; cn > targ; cn--)
+				{
+					printf("%i, ", cn);
+				}
+		}
+
+		if (cn == 98)
+		{
+			printf("98\n");
+		}
+}
