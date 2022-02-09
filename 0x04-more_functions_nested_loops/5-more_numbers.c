826d5872260756ba829774f5fdf015f8b1d78822
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-09 11:23:57 -0800

    5 corrected

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -34,11 +34,18 @@ int rev(int a)
 {
 	int rem, rev = 0;
 
-	while (a != 0)
+	if (a == 0)
 	{
-		rem = a % 10;
-		rev = rev * 10 + rem;
-		a /= 10;
+		return (0);
 	}
-	return (rev);
+	else
+	{
+		while (a != 0)
+		{
+			rem = a % 10;
+			rev = rev * 10 + rem;
+			a /= 10;
+		}
+		return (rev);
+	}
 }
