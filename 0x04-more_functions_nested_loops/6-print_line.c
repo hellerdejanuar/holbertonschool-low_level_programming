9989f2295c91512f0c1253eda849d43edb18fd6d
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-09 13:00:21 -0800

    5 updated

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints spaces ending in a backslash
+ * print_line - prints spaces ending in a backslash
  * @n: number of dashes
  */
 
@@ -10,12 +10,7 @@ void print_line(int n)
 	int cn;
 
 	for (cn = 0; cn < n; cn++)
-	{
 		_putchar('_');
-	}
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
