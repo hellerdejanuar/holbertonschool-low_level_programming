76f64b96ddc5e98829ed876bcc99100b2f3f2f0c
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-11 15:42:45 -0800

    3 added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @str: string to be evaluated
+ */
+void _strlen(char *str)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		_putchar(s[a]);
+	}
+}
