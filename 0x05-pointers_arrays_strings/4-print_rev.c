13254c013af52e8a9939a531a3818d83a16d6b05
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-11 15:57:51 -0800

    4 added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - ptrint string backwards
+ * @s: string to be printed backwards
+ */
+void print_rev(char *s)
+{
+	int a;
+	size_t size = sizeof(s)
+
+	for (a = size - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\0');
+	_putchar('\n');
+}
