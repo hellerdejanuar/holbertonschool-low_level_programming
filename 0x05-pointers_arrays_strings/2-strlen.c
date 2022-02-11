ac24b9026a36d03db725c4b83e6eb5a504bad743
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-11 14:52:36 -0800

    2 added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns length of string
+ * @s: string to be evaluated
+ * Return: x = string length
+ */
+int _strlen(char *s)
+{
+	x = strlng(*s);
+	return (x);
+}
