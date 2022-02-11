ea00abec0d1ddda963350a47a2aa91e4d7909ee2
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-11 15:37:50 -0800

    3 updated

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,15 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strlen - returns length of string
  * @s: string to be evaluated
- * Return: x = string length
+ * Return: string length
  */
 int _strlen(char *s)
 {
-	const char *a = *s;
-	return (strlen(*a));
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{}
+	return (a);
 }
