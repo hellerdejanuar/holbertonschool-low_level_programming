c4eabc980fa193321e3842e02122c2e87b70c843
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-16 13:51:43 -0800

    6 update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters to uppercase
+ * string_toupper - changes first word's lowercase letters to uppercase
  * @str: string to be modified
  * Return: string with all uppers
  */
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
 	int n = 0,x;
 	char sepr[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	if (str[n] >= 'a' && str[n] <= 'z')
-		strn[n] = str[n] - 32;
+		str[n] = str[n] - 32;
 
 	else 
 		for (n = 1; str[n] != '\0'; n++)
 		{
 			for (x = 0; x < 13; x++)
 			{
-				if (str[n - 1] == x)
+				if (str[n - 1] == sepr[x])
 				{
 					if (str[n] >= 'a' && str[n] <= 'z')
 					{
-						strn[n] = str[n] - 32;
+						str[n] = str[n] - 32;
 					}
-					continue
+					continue;
 				}
 			}
 		}
