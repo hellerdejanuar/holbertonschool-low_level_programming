7d859d7400a9f4d9d119e348e93149fe8ed1ad06
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-16 10:11:36 -0800

    3 modified

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcmp - compares two strings
@@ -9,14 +10,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+
+	for (;*s1 == *s2; s1++, s2++)
 	{
-		if (*s1 == '\0')
+		if (*s1 == '\0' && *s2 == '\0')
 			return (0);
-
-		s1++;
-		s2++;
 	}
-
-	return (s1 - s2);
+	return (*s1 - *s2);
 }
