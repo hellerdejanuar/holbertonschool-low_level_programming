b9bb0113da0395b06eb4b8ba305e6851c99766d7
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-16 06:23:22 -0800

    2 added

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies source to dest strin. n sets limit
+ * @dest: destination
+ * @src: source
+ * @n: max number of chars to be printed
+ * Return: concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int sizeDest, sizeSrc, a;
+
+	sizeSrc = _strlen(src);
+	sizeDest = _strlen(dest);
+
+	for (a = 0; a <= sizeSrc && a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+
+	return (dest);
+}
+
+/**
+ * _strlen - takes a string and outputs the number of chars
+ * @str: string to be counted
+ * Return: number of chars
+ */
+int _strlen(char *str)
+{
+	int cn = 0;
+
+	while (str[cn] != '\0')
+		cn++;
+
+	return (cn);
+}
