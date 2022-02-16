64b649bb1650ede34c28abefcc6b01d9bb1bbcea
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-16 10:03:33 -0800

    3 updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,31 +4,19 @@
  * _strcmp - compares two strings
  * @s1: source 1
  * @s2: source 2
- * Return: 0 if both sources are equal, negative int if s1 is less than s2, 
+ * Return: 0 if both sources are equal, negative int if the first distinct char is is less than that of 2, 
  * positive if it's greater.
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int s1sz, s2sz;
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (0);
 
-	s1sz = _strlen(s1);
-	s2sz = _strlen(s2);
+		s1++;
+		s2++;
+	}
 
-	return (s1sz - s2sz);
-}
-
-/**
- * _strlen - takes a string and outputs the number of chars
- * @str: string to be counted
- * Return: number of chars
- */
-int _strlen(char *str)
-{
-	int cn = 0;
-
-	while (str[cn] != '\0')
-		cn++;
-
-	return (cn);
+	return (s1 - s2);
 }
