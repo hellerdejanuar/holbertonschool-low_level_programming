6851ef562b5b4341928411499708fa3748df72aa
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-16 12:50:35 -0800

    5 update

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,7 +7,7 @@ char *string_toupper(char *str)
 {
 	int n;
 
-	while (str[n] != '\0')
+	for (n = 0; str[n] != '\0', n++)
 	{
 		if (str[n] >= 'a' && str[n] <= 'z')
 			str[n] = str[n] + 32;
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,5 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 int _arrlen(int *arr);
 void reverse_array(int *a, int n);
+char *string_toupper(char *);
 #endif /* main.h */
