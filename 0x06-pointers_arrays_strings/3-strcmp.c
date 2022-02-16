2c18f7504a3310152ad3e7a40186cec9abd202df
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-16 10:14:08 -0800

    3 update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,13 +5,13 @@
  * _strcmp - compares two strings
  * @s1: source 1
  * @s2: source 2
- * Return: 0 if both sources are equal, negative int if the first distinct char is is less than that of 2, 
- * positive if it's greater.
+ * Return: 0 if both sources are equal, negative int if the first distinct
+ * char is is less than that of 2, positive if it's greater.
  */
 int _strcmp(char *s1, char *s2)
 {
 
-	for (;*s1 == *s2; s1++, s2++)
+	for (; *s1 == *s2; s1++, s2++)
 	{
 		if (*s1 == '\0' && *s2 == '\0')
 			return (0);
