b786f56f7cb8197cb1ddf18eea84ed2523caed89
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-16 18:49:01 -0800

    7 added

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - changes first word's lowercase letters to uppercase
+ * @str: string to be modified
+ * Return: string with all uppers
+ */
+char *cap_string(char *str)
+{
+	int n = 0, x;
+	char lat[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char leet[] = {4, 3, 0, 7, 1}
+
+	else
+		for (n = 1; str[n] != '\0'; n++)
+		{
+			for (x = 0; x < 13; x++)
+			{
+				if (lat[x] == str[n])
+				{
+					str[n] = leet[x / 2];
+				}
+			}
+		}
+
+	return (str);
+}
