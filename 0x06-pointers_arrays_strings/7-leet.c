5d995f0e1e367f70dc23612bf24a7a8e28a57c60
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-16 18:51:38 -0800

    7 updated

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,17 +11,16 @@ char *cap_string(char *str)
 	char lat[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char leet[] = {4, 3, 0, 7, 1}
 
-	else
-		for (n = 1; str[n] != '\0'; n++)
+	for (n = 1; str[n] != '\0'; n++)
+	{
+		for (x = 0; x < 13; x++)
 		{
-			for (x = 0; x < 13; x++)
+			if (lat[x] == str[n])
 			{
-				if (lat[x] == str[n])
-				{
-					str[n] = leet[x / 2];
-				}
+				str[n] = leet[x / 2];
 			}
 		}
+	}
 
 	return (str);
 }
