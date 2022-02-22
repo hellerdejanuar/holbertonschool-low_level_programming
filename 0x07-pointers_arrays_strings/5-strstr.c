d9715383f24472d614c8f42329785aff19874f7c
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-21 17:27:13 -0800

    5 added

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - returns pointer to first occurance of substring needle
+ * in string haystack
+ * @needle: contains the reference characters.
+ * @haystack: characters to be compared.
+ * Return: pointer to first occurance of needle in haystack
+ */
+char *_strstr(char *haystack, char *needle);
+{
+	int i, j;
+
+	for (i = 0; *(haystack + i) != '\0'; i++)
+	{
+		if (*(haystack + i) == *(needle))
+		{
+			for (j = 1; ; j++)
+			{
+				if (*(haystack + i + j) == *(needle + j))
+					continue;
+
+				else if (*(needle + j) == '\0')
+					return (haystack + i);
+
+				else
+					break;
+			}
+		}
+	}
+
+	return ('\0');
+}
