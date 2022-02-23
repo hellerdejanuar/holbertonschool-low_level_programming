483d6a32b3707c580af24ba49b06e3b4010a897c
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-23 13:14:55 -0800

    5 added

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,13 +7,18 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (iterate (0, n));
+	return (iterate(0, n));
 }
 
+/**
+ * iterate - iterates through ints untill it reaches n
+ * @x: numbers to multiply together
+ * @n: target number
+ */
 int iterate(int x, int n)
 {
 	if (x * x < n)
-		return (iterate (x + 1, n));
+		return (iterate(x + 1, n));
 
 	else if (x * x == n)
 		return (x);
