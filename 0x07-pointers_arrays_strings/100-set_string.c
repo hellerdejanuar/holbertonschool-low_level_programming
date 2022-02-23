4098016bf4baac54d8a20c24bfeff3b3d006d86e
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-22 17:16:09 -0800

    100 added

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of one char to the other
+ * @s: source char
+ * @to: char to be assigned
+ */
+void set_string(char **s, char *to)
+{
+	*to = **s;
+}
