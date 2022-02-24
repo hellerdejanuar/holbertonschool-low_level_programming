d119bc5a8dea0fad9232004869a1518f45b23461
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-23 18:57:42 -0800

    100 updated

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,37 +1,16 @@
 #include "main.h"
 
 /**
- * is_prime_number - calculates if int n is prime
- * @n: number to be evaluated
- * Return: 1 if it is prime, 0 if it doesn't
+ * is_palindrome - is palindrome
+ * @s: string to be evaluated
+ * Return: 1 if it is paliundrome, 0 if it doesn't
  */
 int is_palindrome(char *s)
 {
-	if (n <= 1)
+	if (s == 'a')
 		return (0);
 
 	else
-		return (iterate_prime(2, n));
-}
-
-/**
- * iterate_prime - iterates through ints untill it
- * reaches a prime or end of list
- * @i: number being tested to be a divisor of n
- * @n: number to evaluate
- * Return: 1 for prime 0 otherwise -1 error
- */
-int iterate_prime(int i, int n)
-{
-	if (n % i == 0)
-		return (0);
-
-	else if (i >= n / 2)
 		return (1);
-
-	else if (n % i != 0)
-		return (iterate_prime(i + 1, n));
-
-	else
-		return (-1);
 }
+
