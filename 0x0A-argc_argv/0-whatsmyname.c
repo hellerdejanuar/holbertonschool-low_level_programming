ba96405829b8295ea65de3923d53c1daafd182be
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-25 04:49:16 -0800

    0 style corrected

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - prints name of the function
+ * Return: 0 Success
+ */
 int main(int argc __attribute__((unused)),char *argv[])
 {
 	printf("%s\n", argv[0]);
