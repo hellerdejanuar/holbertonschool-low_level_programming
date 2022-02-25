13f7801f15809e52fb9e3011e5a3a637b793ea75
hellerdejanuar <hellerdejanuar@gmail.com>
2022-02-25 10:30:21 -0800

    0 style corrected

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: argument vector
  * Return: 0 Success
  */
-int main(int argc __attribute__((unused)),char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
