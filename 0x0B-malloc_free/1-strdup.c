ffc013a4f3991245840f03dc3d702f1789d361c0
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-02 08:47:00 -0800

    1 added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - copies string to newly dinamically allocated array
+ * @str: string to be copied
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, size;
+	char *ar;
+
+	if (str == NULL)
+		return (NULL);
+
+	else
+	{
+		for (size = 0; str[size] != '\0'; size++)
+
+		ar = malloc(size * sizeof(char));
+
+		if (ar == NULL)
+			return (NULL);
+
+		else
+		{
+			for (i = 0; i < size ; i++)
+				ar[i] = str[i];
+
+			return (ar);
+		}
+	}
+}
