0ceab3997eeb454630ccdce7663583981aea2e7c
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-02 12:22:55 -0800

    2 update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
 #include <stdio.h>
-
 /**
  * str_concat - concatenates string to newly dinamically allocated array
  * @s2: string 2
@@ -9,37 +8,38 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 1, j = 1, x;
+	int i, j, x;
 	char *ar;
-
 	if (s1 == NULL)
+	{
 		s1 = "";
-
+		i = 1;
+	}
 	else
 		for (i = 0; s1[i] != '\0'; i++)
-
+	
 	if (s2 == NULL)
+	{
 		s2 = "";
+		j = 1;
+	}
 
 	else
 		for (j = 0; s2[j] != '\0'; j++)
-
 
 	ar = malloc((i + j + 1) * sizeof(char));
 
 	if (ar == NULL)
 		return (NULL);
-
 	else
 	{
 		for (x = 0; s1[x] != 0; x++)
 			ar[x] = s1[x];
-		
+
 		for (x = 0; s2[x] != 0; x++)
 			ar[x + i] = s2[x];
 
 		ar[j + i] = '\0';
-
 		return (ar);
 	}
 }
