cf60892fffd086a6b840ca2fe0480e86c882bbf9
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-02 10:28:08 -0800

    2 updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strdup - copies string to newly dinamically allocated array
- * @str: string to be copied
+ * str_concat - concatenates string to newly dinamically allocated array
+ * @s1: string 1
+ * @s2: string 2
  * Return: pointer to new string
  */
 char *str_concat(char *s1, char *s2)
@@ -11,31 +13,29 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, x;
 	char *ar;
 
-/*
- *	*blank = '\0'
- *
- * 	if (s1 == NULL)
- * 	s1 = blank
- */
-		for (i = 0; s1[i] != '\0'; i++)
 
-		for (j = 0; s2[j] != '\0'; j++)
+	for (i = 0; s1[i] != '\0'; i++)
 
-		j++;
+	for (j = 0; s2[j] != '\0'; j++)
 
-		ar = malloc((i + j + 1) * sizeof(char));
+	j++;
 
-		if (ar == NULL)
-			return (NULL);
+	ar = malloc((i + j + 1) * sizeof(char));
 
-		else
+	if (ar == NULL)
+		return (NULL);
+
+	else
+	{
+		for (x = 0; x < i; x++)
 		{
-			for (x = 0; x < i; x++)
-				ar[x] = s1[x];
-
-			for (x = 0; x < j; x++)
-				ar[x + i] = s2[x];
-
-			return (ar);
+			printf("debug\n");
+		/*	ar[x] = s1[x];*/
 		}
+
+		for (x = 0; x < j; x++)
+			ar[x + i] = s2[x];
+
+		return (ar);
+	}
 }
