af24891b8a40e2c54ba3ff64b5bb0f4c950f918d
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-02 09:41:31 -0800

    2 added

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - copies string to newly dinamically allocated array
+ * @str: string to be copied
+ * Return: pointer to new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j, size;
+	char *ar;
+
+	if (str == NULL)
+		return (NULL);
+
+	else
+	{
+		for (size = 0; s1[size] != '\0'; size++)
+		
+		for (; s2[size] != '\0'; size++)
+
+		size++;
+
+		ar = malloc((size + 1) * sizeof(char));
+
+		if (ar == NULL)
+			return (NULL);
+
+		else
+		{
+			for (i = 0; s1[i] != '\0'; i++)
+				ar[i] = s1[i];
+
+			for (j = 0; i < size; i++, j++)
+				ar[i] = s2[j];
+
+			return (ar);
+		}
+	}
+}
