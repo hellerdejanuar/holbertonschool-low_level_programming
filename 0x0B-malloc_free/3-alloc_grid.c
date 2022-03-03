5ecfdbc8fcd0dc7fdf78d8797e00a0567f93a6c2
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-02 16:51:55 -0800

    3 updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -35,15 +35,14 @@ int **alloc_grid(int width, int height)
 		}
 
 
+		for (j = 0; j < width; j++)
 		{
-			for (j = 0; j < width; j++)
+			for (i = 0; i < height; i++)
 			{
-				for (i = 0; i < height; i++)
-				{
-					grid[i][j] = 0;
-				}
+				grid[i][j] = 0;
 			}
-			return (grid);
 		}
+
+		return (grid);
 	}
 }
