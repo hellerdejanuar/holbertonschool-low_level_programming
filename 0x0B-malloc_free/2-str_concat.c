a7d05dc068e13fc1b7dc3bb364531b4ec4d4181b
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-04 06:13:15 -0800

    2 updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,43 +3,43 @@
 #include <stdio.h>
 /**
  * str_concat - concatenates string to newly dinamically allocated array
+ * @s1: string 1
  * @s2: string 2
  * Return: pointer to new string
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, x;
-	char *ar;
+	char *str;
+	int i = 0, j = 0;
+
 	if (s1 == NULL)
-	{
 		s1 = "";
-		i = 1;
-	}
-	else
-		for (i = 0; s1[i] != '\0'; i++)
-	
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-		j = 1;
-	}
 
-	else
-		for (j = 0; s2[j] != '\0'; j++)
+	do {
+		i++;
+	} while (s1[i])
 
-	ar = malloc((i + j + 1) * sizeof(char));
+	do {
+	   j++;
+	} while (s2[j])
 
-	if (ar == NULL)
+	str = malloc((i + j + 1) * sizeof(char));
+
+	if (str == NULL)
 		return (NULL);
-	else
+
+	for (x = 0; x < i; x++)
 	{
-		for (x = 0; s1[x] != 0; x++)
-			ar[x] = s1[x];
-
-		for (x = 0; s2[x] != 0; x++)
-			ar[x + i] = s2[x];
-
-		ar[j + i] = '\0';
-		return (ar);
+		str[x] = s1[x];
 	}
+
+	for (x = 0; x < j; x++)
+	{
+		str[x + i - 1] = s2[x];
+	}
+
+	return (str);
 }
---
+++ 0x0B-malloc_free/2-str_concat_v3.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * str_concat - concatenates string to newly dinamically allocated array
+ * @s2: string 2
+ * Return: pointer to new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, x;
+	char *ar;
+	if (s1 == NULL)
+	{
+		s1 = "";
+		i = 1;
+	}
+	else
+		for (i = 0; s1[i] != '\0'; i++)
+	
+	if (s2 == NULL)
+	{
+		s2 = "";
+		j = 1;
+	}
+
+	else
+		for (j = 0; s2[j] != '\0'; j++)
+
+	ar = malloc((i + j + 1) * sizeof(char));
+
+	if (ar == NULL)
+		return (NULL);
+	else
+	{
+		for (x = 0; s1[x] != 0; x++)
+			ar[x] = s1[x];
+
+		for (x = 0; s2[x] != 0; x++)
+			ar[x + i] = s2[x];
+
+		ar[j + i] = '\0';
+		return (ar);
+	}
+}
