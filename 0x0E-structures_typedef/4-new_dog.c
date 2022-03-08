0d7ca7eb3fafaf7192d5bc893d146eb9a90d894c
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-08 14:07:33 -0800

    4 added

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new instance of dog_t
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: dog_t type pointer to dog_t instance
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i;
+	dog_t *new_dog = NULL;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	(*new_dog).age = age;
+
+	(*new_dog).name = malloc(sizeof(name));
+		if ((*new_dog).name == NULL)
+			return (NULL);
+
+	for (i = 0; name[i]; i++)
+		(*new_dog).name[i] = name[i];
+
+	(*new_dog).owner = malloc(sizeof(owner));
+		if ((*new_dog).owner == NULL)
+			return (NULL);
+
+	for (i = 0; owner[i]; i++)
+		(*new_dog).owner[i] = owner[i];
+
+	return (new_dog);
+}
