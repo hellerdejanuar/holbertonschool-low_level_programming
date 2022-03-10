1fe8bec92b5894d69d812808be32298b5296fa1d
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-09 17:43:39 -0800

    3 updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,14 +12,14 @@ int main (int argc, char *argv[])
 	if (argc != 4)
 		return (98);
 
-	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
-		    && argv[2][0] != '/' && argv[2][0] != '%') || argv[2][1] != '\0')
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
+		&& argv[2][0] != '/' && argv[2][0] != '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit (99);
 	}
 
-	if ((argv[2] == '/' || argv[2]) && argv[3] == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == 0)
 	{
 		printf("Error\n");
 		exit (99);
