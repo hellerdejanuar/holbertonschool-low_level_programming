6e83bc7159e9a32454eb3ec336085b16071d3e80
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-17 15:50:30 -0700

    1 added

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list - prints out a list of strings
+ * @h: list
+ *
+ * Return: Number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n_cn = 0;
+
+	while (h != NULL)
+	{
+		h = (*h).next;
+		n_cn++;
+	}
+
+	return (n_cn);
+}
