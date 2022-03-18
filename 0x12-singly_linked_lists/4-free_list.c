201ecdecefa7285bb3cf36bbed8b9701e5aceb9c
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-18 11:20:01 -0700

    4 added working

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees all nodes of a list
+ * @head: pointer to the first node
+ */
+void free_list(list_t *head)
+{
+	list_t *aux;
+
+	while ((*head).next != NULL)
+	{
+		aux = head;
+		head = (*head).next;
+		free(aux);
+	}
+
+	free(head);
+}
