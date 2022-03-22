8e998a3d071f2cb2cce3a0673a633798fac21afe
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-22 13:40:39 -0700

    3 style corrected

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,11 +10,12 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *last_node;
+
 	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	(*new_node).n = n;
 	(*new_node).next = NULL;
 
@@ -23,7 +24,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		last_node = *head;
 		while ((*last_node).next != NULL)
 			last_node = (*last_node).next;
-		
+
 		(*last_node).next = new_node;
 	}
 	else
