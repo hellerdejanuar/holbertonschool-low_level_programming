59fc6acfe8ff8eaa6b23b242ebd69b7d8c3120a2
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-22 15:39:25 -0700

    5 added

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees all nodes of a list
+ * @head: pointer to the first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	while (*head != NULL)
+	{
+		aux = *head;
+		*head = (**head).next;
+		free(aux);
+	}
+}
