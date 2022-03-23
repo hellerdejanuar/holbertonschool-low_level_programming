29c64037bcb679339ec361e0f3e1b18c042e937a
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-22 18:20:45 -0700

    5 updated (border case: no input)

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		aux = *head;
 		*head = (**head).next;
 		free(aux);
 	}
-	free(*head);
 }
