d62aac0c098a1e17c24a0152f8f567e87e6b7cb5
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-23 07:53:46 -0700

    readme added

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of the given list
+ * @head: list
+ * @index: node number
+ *
+ * Return: node. NULL if node noesnt exist
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int cn = 0;
+	listint_t *new_node, *last_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (head == NULL)
+		return (NULL);
+
+	/* if head points to null (empty list) */
+	if (*head == NULL)
+	{
+		if (idx == 0)
+		{
+			new_node->next = NULL;
+			*head = new_node;
+			return (new_node);
+		}
+		else
+			return (NULL);
+	}
+
+	last_node = *head;
+
+	/* find idx th node */
+	while (cn < idx)
+	{
+		if ((*last_node).next != NULL)
+		{
+			last_node = (*last_node).next;
+			cn++;
+		}
+		else
+			return (NULL);
+	}
+
+	new_node->next = last.node->next;
+	last_node->next = new_node;
+
+	if (*head != NULL)
+	{
+	}
+	else
+	{
+		*head = new_node;
+	}
+
+	return (new_node);
+}
