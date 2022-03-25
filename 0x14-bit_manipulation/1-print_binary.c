373d1d3bbe6c6d253453c605fad80c5199acf390
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-25 07:50:55 -0700

    1 updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,7 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int word_sz = sizeof(n) * 4 - 1;
+	long int word_sz = sizeof(n) * 4 - 1;
 	int flag = 0;
 
 	for (; word_sz >= 0; word_sz--)
