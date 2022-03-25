5d6e5dddfa75ac1e146f7a222296f87cad74d3d9
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-25 08:03:14 -0700

    1 back to normal

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,11 +6,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	long int word_sz = sizeof(n) * 4 - 1;
+	int word_sz = sizeof(n) * 4 - 1;
 	int flag = 0;
 
 	for (; word_sz >= 0; word_sz--)
-	{i
+	{
 		/* if comparator & n is not 0 then true */
 		if (1 << word_sz & n)
 		{
