3c5343d59123536e5ff24c6ef36690be117d6e1d
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-29 11:07:37 -0700

    1 update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,16 +13,16 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[len] != '\0')
-		len++;
-
 	if (text_content == NULL)
 		text_content = "";
+
+	while (text_content[len] != '\0')
+		len++;
 
 	wr_stat = write(fd, text_content, len);
 
