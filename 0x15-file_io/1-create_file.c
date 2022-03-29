899c71b78ded514bb7054bf665aa73ebbf89fc31
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-29 07:28:25 -0700

    1 update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,6 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, ch_cn = 0;
 	ssize_t wr_stat = 0;
-	char *buffer;
 
 	if (*filename == '\0' || filename == NULL)
 		return (-1);
@@ -20,26 +19,15 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[ch_cn] != '\0')
 		ch_cn++;
 
-	if (ch_cn == 0)
-	{
-		wr_stat = write(fd, "", 0);
-		buffer = NULL;
-	}
-	else
-	{
-		buffer = malloc(sizeof(char) * ch_cn);
-		if (buffer == NULL)
-			return (-1);
-		wr_stat = write(fd, buffer, ch_cn);
-	}
-	
+	if (text_content == NULL)
+		text_content = "";
 
+	wr_stat = write(fd, text_content, ch_cn);
 
 	if (wr_stat == -1 || wr_stat != ch_cn)
 		return (-1);
 
 	close(fd);
-	free(buffer);
 
 	return (1);
 }
