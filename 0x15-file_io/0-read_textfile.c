6df22c32f02bb246e1162075fe2da2fd9aba4352
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-28 20:02:14 -0700

    0 updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,12 +15,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1 || filename == NULL || *filename == '\0' || buffer == NULL)
 		return (0);
 
-	read(fd, buffer, letters);
+	cn = read(fd, buffer, letters);
+	if (cn == -1)
+		return(0);
+
 	buffer[letters] = '\0';
 
 	close(fd);
 
-	cn = write(STDOUT_FILENO, buffer, letters);
+	cn = write(STDOUT_FILENO, buffer, cn);
 	free(buffer);
 
 	if (cn == -1)
