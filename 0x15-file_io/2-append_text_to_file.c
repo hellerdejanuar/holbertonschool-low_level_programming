dbc2841b7a0b8a6dfb855d8bdb2e485b1b27a272
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-29 11:37:40 -0700

    2 updated

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: filename
+ * @text_content: number of letters to be printed
+ * Return: - 1 failed. 1 success)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0;
+	ssize_t wr_stat = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	while (text_content[len] != '\0')
+		len++;
+
+	wr_stat = write(fd, text_content, len);
+
+	if (wr_stat != len)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
