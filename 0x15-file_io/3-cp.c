43d8bb304e3f8840deda025b38ccc2d0745afd07
hellerdejanuar <hellerdejanuar@gmail.com>
2022-03-31 08:58:23 -0700

    2 updated (working with many buffers)

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,28 +8,29 @@
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	int fd_src, fd_trg;
-	ssize_t read_cn = 0, write_cn = 0;
+	ssize_t read_cn = 0, write_cn = 0, r_stat;
 	char buffer[1024];
 
 	fd_src = open(argv[1], O_RDONLY);
 
-	if (fd_src == -1)
+	fd_trg = open(argv[2], O_WRONLY | O_CREAT | O_APPEND, 0600);
+	
+	if (fd_src == -1 || fd_trg == -1)
 		return (-1); /* Open Failed */
 
-	read_cn = read(fd_src, buffer, 1024);
-
-	if (read_cn == -1)
+	for (r_stat = 1024 ; r_stat == 1024 ; read_cn += r_stat)
+	{	
+		r_stat = read(fd_src, buffer, 1024);
+		if (r_stat == -1)
 			return (-1); /* Read failed */
 
+		write_cn = write(fd_trg, buffer, r_stat);
+
+		if (write_cn == -1)
+			return (-1); /* write failed */
+	}
+
 	close(fd_src);
-	
-	fd_trg = open(argv[2], O_WRONLY | O_CREAT, 0600);
-
-	write_cn = write(fd_trg, buffer, read_cn);
-
-	if (write_cn != read_cn)
-		return (-1);
-
 	close(fd_trg);
 
 	return (1);
