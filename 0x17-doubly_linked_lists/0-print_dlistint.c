86b7fa35a44bff89ccde603dd85fba66c9b72d06
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-05 10:09:44 -0700

    0 upd

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all te elements of a dlistint_t list
+ * @h: list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int cn;
+
+	for (cn = 0; h->next != NULL; cn++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	printf("%d\n", h->n);
+	cn++;
+	return (cn);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a project about doubly linked lists
