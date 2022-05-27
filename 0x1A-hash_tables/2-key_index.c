308c8a85fd7bfce7dc91aff7a015824adbaaf9f3
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-27 14:27:53 -0700

    2 style corrected

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - gets hash code for key
+ * @size: size of table
+ * @key: string to be hashed
+ * Return: hash code
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (size == 0)
