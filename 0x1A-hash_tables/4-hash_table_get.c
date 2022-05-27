8d438adab2243cfb90e44c98fac0bd28f8dd7228
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-27 16:25:00 -0700

    4 add

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table you to get the value from
+ * @key: key
+ * Return: Value, NULL if failed
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	long unsigned int idx = 0;
+	hash_node_t *c_node = NULL;
+
+	if (!ht || !key)
+		return (NULL);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	c_node = ht->array[idx];
+
+	while (c_node != NULL)
+	{
+		if (strcmp(c_node->key, key) == 0)
+		{
+			return (c_node->key);
+		}
+		c_node = c_node->next;
+	}
+
+	return (NULL);
+}
