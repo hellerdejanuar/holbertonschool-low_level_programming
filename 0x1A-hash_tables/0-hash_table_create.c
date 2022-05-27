2eadee1bdcae4fc2ddc7ad7bcb7611aeb0461c5a
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-27 14:08:13 -0700

    upd

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,18 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	if (size == 0);
+	hash_table_t *new_table = NULL;
+	
+	if (size == 0)
 		return (NULL);
 
-	hash_table_t *new_table = NULL;
 	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL):
+	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = calloc(hash_node_t *, size);
-	if (new_table->array == NULL):
+	new_table->array = calloc(size, sizeof(hash_node_t *));
+	if (new_table->array == NULL)
 		return (NULL);
 
-	return (new);
+	return (new_table);
 }
