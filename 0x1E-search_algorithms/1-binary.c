76ea6e2ae8e545b35ff6b6bd506cd9280919a08b
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-26 10:59:48 -0700

    1 add

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Finds the first occurance of value in array
+ * @array: array
+ * @size: size of the array
+ * @value: value to search for
+ * Return: Index if found. -1 if Failure
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t j; 
+	size_t i = 0; 
+	size_t upper = size; 
+	size_t lower = 0;
+
+	if (!array) 
+		return (-1);
+
+	while (lower <= upper)
+	{
+		printf("Searching in array: ");
+
+		for (j = lower; j < upper ; j++)
+		{
+			printf("%d", array[j]);
+			if (j < upper - 1)
+				printf(", ");
+			else
+				printf("\n");
+		}
+
+		i = lower + (upper - lower) / 2;
+		if (array[i] == value)
+			return (i);
+		else if (array[i] < value)
+			lower = i + 1;
+		else if (array[i] > value)
+			upper = i - 1;
+	}
+	return (-1);
+}
